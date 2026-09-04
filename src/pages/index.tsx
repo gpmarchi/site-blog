b11c567192648c79625977f07742de1d5291f889
gpmarchi <gustavomarchi@gmail.com>
2026-09-04 16:10:54 -0300

    refactor: downgrade tailwind to version 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+      className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"
     >
-      <h2>Hello, Next.js!</h2>
+      <h2 className="text-5xl text-red-500">Hello, Next.js!</h2>
     </div>
   );
 }
